3cc055e72a2c18ae030e47434de971ae47f47a4f
Will Cameron <william.cameron@emergencyreporting.com>
2016-09-28 17:13:14 -0700

    added a stepper for examples

--- src/App.js
+++ src/App.js
@@ -1,40 +1,66 @@
 import React, { Component } from 'react';
 import './App.css';
 import Grid from './Components/Grid';
+import SetStateExample from './Components/SetStateExample';
 // import data from '../db.json';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      timeUp: 0
+      currentStep: 0
     };
   }
 
-  componentDidMount() {
-    setInterval(() => {
-      this.setState({
-        timeUp: this.state.timeUp + 1
-      })
-    }, 1000)
-  }
   render() {
     const { products, transactions } = this.props.data;
 
     return (
       <div className="container-fluid">
         <div className="App-header row">
-          <div className="col-md-12">
+          <div className="col-md-1">
+            {this.state.currentStep > 0 ? <button onClick={ () => this.setState({ currentStep: this.state.currentStep - 1 })} className="button navigation-btn">Previous</button> : null}
+          </div>
+          <div className="col-md-10">
             <h2 style={{color:"#fff"}}>
               <span style={{color:'#de4343'}}>Emergency</span> <span style={{color:'orange'}}>Reporting</span> React Training
             </h2>
           </div>
+          <div className="col-md-1">
+            <button onClick={ () => this.setState({ currentStep: this.state.currentStep + 1 })} className="button navigation-btn">Next</button>
+          </div>
         </div>
-        <p>Uptime: {this.state.timeUp}</p>
-        <Grid rows={products} childRows={transactions} />
+
+        <Stepper step={this.state.currentStep}>
+          <SetStateExample />
+          <Grid rows={products} childRows={transactions} />
+          <div>Thanks!</div>
+        </Stepper>
+
       </div>
     );
   }
 }
 
+class Stepper extends Component {
+  constructor(props) {
+    super(props);
+      this.state = {
+        step: props.step
+      }
+  }
+
+  componentWillReceiveProps(nextProps) {
+    this.setState({
+      step: nextProps.step
+    })
+  }
+  render() {
+    return (
+      <div>
+        {this.props.children[this.state.step]}
+      </div>
+    );
+  }
+}
 export default App;
